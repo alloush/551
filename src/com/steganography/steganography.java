52f8446cce725b3c986c11d37e962d4836b42606
Ali J <metroidmoo@gmail.com>
2011-11-20 19:50:19 -0500

    Changed Stenography.java's code to something more suitable

--- src/com/steganography/steganography.java
+++ src/com/steganography/steganography.java
@@ -1,66 +1,43 @@
 package com.steganography;
 
-import java.io.File;
-import android.app.Activity;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.Toast;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class steganography extends Activity {
     
     private static final int SELECT_PICTURE = 1;
     private String selectedImagePath;
     private ImageView img;
+    private static final int SELECT_IMAGE = 0; // selector for image gallery call
     
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        img = (ImageView) findViewById(R.id.ImageView01);
+        
+                setContentView(R.layout.main);
         ((Button) findViewById(R.id.Button01))
                 .setOnClickListener(new OnClickListener() {
 
                     public void onClick(View arg0) {
-                        Intent intent = new Intent();
-                        intent.setType("image/*");
-                        intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(
-                                Intent.createChooser(intent, "Select Picture"),
-                                SELECT_PICTURE);
+                        startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), SELECT_IMAGE);
                     }
                 });
     }
-
+    
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
-                System.out.println("Image Path : " + selectedImagePath);
-                img.setImageURI(selectedImageUri);
-            }
-        }
-    }
-
-    public String getPath(Uri uri) {
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        int column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
+      super.onActivityResult(requestCode, resultCode, data);
+      if (requestCode == SELECT_IMAGE)
+        if (resultCode == Activity.RESULT_OK) {
+          Uri selectedImage = data.getData();
+          // TODO Do something with the select image URI
+        } 
     }
 }
+
+   
